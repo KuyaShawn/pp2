605fec7347030557fc423df5b7dd2634a1e92640
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-04-15 12:49:13 -0400

    added a functions, headers, title, and printed the array

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+
+<?php
+    echo "<title>Pair Program 2</title>";
+    /*
+     * Eashune Abenojar
+     * 4/15/21
+     * https://github.com/KuyaShawn/pp2.git
+     * https://eabenojar.greenriverdev.com/328/pp2/index.php
+    */
+    echo "<h1>Pair Program 2</h1>";
+    echo "<h2>PHP Array Practice.</h2>";
+
+    $numbers = [ 7, 9, 8, 9, 8, 8, 6];
+    function printArry($numbers){
+        $arrayLength = count($numbers);
+        for($i = 0; $i < $arrayLength; $i++){
+            echo "<p>$numbers[$i]</p>";
+        }
+    }
+    printArry($numbers);
+
+?>
+
+</body>
+</html>
